800b09fbcb6f9c2e35e2150262c5ffa2a8006a78
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-04-24 23:25:37 -0400

    Initial implementation of waypoints

---
+++ addon/utils/check-scroll-direction.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+const { floor } = Math;
+
+export default function checkScrollDirection(lastPosition = null, newPosition = {}, sensitivity = 1) {
+  if (!lastPosition) { return 'none'; }
+
+  Ember.assert('sensitivity cannot be 0', sensitivity);
+
+  const { top, left } = newPosition;
+  const { top: lastTop, left: lastLeft } = lastPosition;
+
+  const delta = {
+    top  : floor((top  - lastTop)  / sensitivity) * sensitivity,
+    left : floor((left - lastLeft) / sensitivity) * sensitivity
+  };
+
+  if (delta.top  > 0) { return 'down'; }
+  if (delta.top  < 0) { return 'up'; }
+  if (delta.left > 0) { return 'right'; }
+  if (delta.left < 0) { return 'left'; }
+}
