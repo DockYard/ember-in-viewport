53f607c825595995d1acc10cde7a6872314f7bae
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-04-29 13:20:00 -0400

    Set default value for `viewportConfig`.
    Prevents an error when `viewportConfig` is not defined in the app's
    config.

--- app/initializers/viewport-config.js
+++ app/initializers/viewport-config.js
@@ -1,7 +1,7 @@
 import config from '../config/environment';
 
 export function initialize(_container, application) {
-  const { viewportConfig } = config;
+  const { viewportConfig = {} } = config;
 
   application.register('config:in-viewport', viewportConfig, { instantiate: false });
 }
