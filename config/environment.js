870d50633c22766d62d4547944bfad5d3ce6eab3
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-05-03 21:44:18 -0400

    Move default config to initializer.
    This prevents an issue where a consuming app's config would not be
    deeply merged with the default config, which would make some props
    undefined.

--- config/environment.js
+++ config/environment.js
@@ -3,18 +3,5 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
-  return {
-    viewportConfig: {
-      viewportSpy               : false,
-      viewportScrollSensitivity : 1,
-      viewportRefreshRate       : 100,
-      viewportListeners         : [],
-      viewportTolerance: {
-        top    : 0,
-        left   : 0,
-        bottom : 0,
-        right  : 0
-      },
-    }
-  };
+  return {};
 };
