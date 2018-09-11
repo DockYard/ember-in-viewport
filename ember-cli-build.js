c8ccb9d5e5127617d7ead66bc373c3c2a5b48fc8
Scott Newcomer <snewcomer@users.noreply.github.com>
2018-09-10 21:04:34 -0700

    Update to 3.4 and resolve audit warning (#163)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,12 +3,9 @@
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  let project = defaults.project;
-  let options = {};
-  if (project.findAddonByName('ember-native-dom-event-dispatcher')) {
-    options.vendorFiles = { 'jquery.js': null };
-  }
-  let app = new EmberAddon(defaults, options);
+  let app = new EmberAddon(defaults, {
+    vendorFiles: { 'jquery.js': null }
+  });
 
   /*
     This build file specifies the options for the dummy test app of this
