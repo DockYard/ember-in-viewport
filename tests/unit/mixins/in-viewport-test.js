8e50b37080017166b734618865f5f2eebbefe631
Scott Newcomer <snewcomer@users.noreply.github.com>
2018-11-27 12:46:19 -0800

    Use rAFPool npm pkg (#170)

--- tests/unit/mixins/in-viewport-test.js
+++ tests/unit/mixins/in-viewport-test.js
@@ -2,17 +2,13 @@ import EmberObject from '@ember/object';
 import InViewportMixin from 'ember-in-viewport/mixins/in-viewport';
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-import rAFPoolManager from 'ember-in-viewport/services/-raf-admin';
-
-class rAFMock extends rAFPoolManager {
-  flush() {}
-}
+import RafPool from 'raf-pool';
 
 module('Unit | Mixin | in viewport', function(hooks) {
   setupTest(hooks);
 
   hooks.beforeEach(function() {
-    this.rAFPoolManager = new rAFMock();
+    this.rAFPoolManager = new RafPool();
   });
 
   test('mixin works', function(assert) {
@@ -22,29 +18,33 @@ module('Unit | Mixin | in viewport', function(hooks) {
   });
 
   test('can add to pool manager', function(assert) {
-    let obj = this.rAFPoolManager.add(123, 'wat');
-    assert.equal(obj, 'wat');
+    let cb = () => {};
+    let obj = this.rAFPoolManager.add(123, cb);
+    assert.equal(obj, cb);
   });
 
   test('can remove from pool manager', function(assert) {
-    this.rAFPoolManager.add(123, 'wat');
+    let cb = () => {};
+    this.rAFPoolManager.add(123, cb);
     assert.equal(this.rAFPoolManager.pool.length, 1);
     this.rAFPoolManager.remove(123);
     assert.equal(this.rAFPoolManager.pool.length, 0);
   });
 
   test('can remove from pool manager', function(assert) {
-    this.rAFPoolManager.add(123, 'wat');
-    this.rAFPoolManager.add(124, 'foo');
+    let cb = () => {};
+    this.rAFPoolManager.add(123, cb);
+    this.rAFPoolManager.add(124, cb);
     assert.equal(this.rAFPoolManager.pool.length, 2);
     this.rAFPoolManager.remove(123);
     assert.equal(this.rAFPoolManager.pool.length, 1);
-    assert.equal(this.rAFPoolManager.pool[0]['124'], 'foo');
+    assert.equal(this.rAFPoolManager.pool[0]['124'], cb);
   });
 
   test('reset resets pool manager', function(assert) {
-    this.rAFPoolManager.add(123, 'wat');
-    this.rAFPoolManager.add(124, 'foo');
+    let cb = () => {};
+    this.rAFPoolManager.add(123, cb);
+    this.rAFPoolManager.add(124, cb);
     assert.equal(this.rAFPoolManager.pool.length, 2);
     this.rAFPoolManager.reset();
     assert.equal(this.rAFPoolManager.pool.length, 0);
