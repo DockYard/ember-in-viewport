5ca197a2ca4e9743974c8935f95ecd321631a9c6
snewcomer <snewcomer24@gmail.com>
2019-04-21 06:43:19 -0700

    change dummy component to class

--- tests/dummy/app/components/my-class.js
+++ tests/dummy/app/components/my-class.js
@@ -1,18 +1,18 @@
 import Component from '@ember/component';
-import { inject } from '@ember/service';
+import { tagName } from '@ember-decorators/component';
+import { inject as service } from '@ember/service';
 
-export default Component.extend({
-  inViewport: inject(),
-
-  tagName: '',
+@tagName('')
+export default class MyClass extends Component {
+  @service inViewport
 
   didInsertElement() {
     const loader = document.getElementById('loader');
     this.inViewport.watchElement(loader);
     this.inViewport.addEnterCallback(loader, this.didEnterViewport.bind(this));
-  },
+  }
 
   didEnterViewport() {
     this.infinityLoad();
   }
-});
+}
