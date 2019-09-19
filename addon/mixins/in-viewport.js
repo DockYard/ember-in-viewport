265bdc5066a97bdcac909ab61c49b028442cf716
Kjaer <kayer.halil@gmail.com>
2019-09-19 15:39:32 +0200

    grouping if statement to evaluate correctly

--- addon/mixins/in-viewport.js
+++ addon/mixins/in-viewport.js
@@ -146,7 +146,7 @@ export default Mixin.create({
       const enterCallback = () => {
         const isTearingDown = this.isDestroyed || this.isDestroying;
         const viewportEntered = JSON.parse(element.getAttribute('data-in-viewport-entered'));
-        if (!isTearingDown && viewportSpy || viewportEntered) {
+        if (!isTearingDown && (viewportSpy || viewportEntered)) {
           set(this, 'viewportEntered', true);
           this.trigger('didEnterViewport');
         }
