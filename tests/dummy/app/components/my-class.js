874b094ed843151f56e476f898f495cde5f01737
Scott Newcomer <snewcomer@users.noreply.github.com>
2020-09-25 13:19:10 -0700

    Add Glimmer component for tests (#255)

--- tests/dummy/app/components/my-class.js
+++ tests/dummy/app/components/my-class.js
@@ -1,15 +1,14 @@
-import Component from '@ember/component';
-import { get } from '@ember/object';
-import { tagName } from '@ember-decorators/component';
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
-@tagName('')
 export default class MyClass extends Component {
   @service inViewport
 
-  didInsertElement() {
+  @action
+  setupViewport() {
     const loader = document.getElementById('loader');
-    const { onEnter } = get(this, 'inViewport').watchElement(loader);
+    const { onEnter } = this.inViewport.watchElement(loader);
     onEnter(this.didEnterViewport.bind(this));
   }
 
@@ -19,6 +18,6 @@ export default class MyClass extends Component {
 
   willDestroyElement() {
     const loader = document.getElementById('loader');
-    get(this, 'inViewport').stopWatching(loader);
+    this.inViewport.stopWatching(loader);
   }
 }
