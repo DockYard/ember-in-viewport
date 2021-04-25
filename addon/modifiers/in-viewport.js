50c457f06f2f3296821c875a19e9c4578336c7d4
scott-newcomer <snewcomer24@gmail.com>
2021-04-25 11:24:56 -0500

    mv around

--- addon/modifiers/in-viewport.js
+++ addon/modifiers/in-viewport.js
@@ -41,16 +41,16 @@ export default class InViewportModifier extends Modifier {
     if (this.args.named.onEnter) {
       this.args.named.onEnter.call(null, this.element);
     }
+
+    if (!this.options.viewportSpy) {
+      this.inViewport.stopWatching(this.element);
+    }
   }
 
   @action
   onExit() {
     if (this.args.named.onExit) {
       this.args.named.onExit.call(null, this.element);
-    }
-
-    if (!this.options.viewportSpy) {
-      this.inViewport.stopWatching(this.element);
     }
   }
 
