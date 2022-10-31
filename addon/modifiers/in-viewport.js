479b87c51e87ba0d8ef7b6ce596ca1d12661dcab
Andranik Tsaturyan <62332020+And012@users.noreply.github.com>
2022-10-31 05:07:42 +0400

    make in-viewport compatible to ember-modifier v4 (#306)
    * make in-viewport compatible to ember-modifier v4
    
    * resolve conversation
    
    * add missing scenarious
    
    * add missing scenarious
    
    * fix-lint
    
    * remove jquery scenario from ember-try and scenario
    
    * make scenario sync with CI
    
    * upgrade @embroider/test-setup
    
    * resolve conversation regarding packages
    
    * install dependencies with npm older version
    
    * revert package-lock.json and install dependencies
    
    * remove line to make ci work
    
    * add embroider macros to version 4 scneario dependancy
    
    * add @embroid/core @embroider/compat @embroider/webpack as dev dpcy
    
    * resolve could find module issue
    
    * revert change in service
    
    Co-authored-by: Andranik Tsaturyan <tsaturyana@ae.com>

--- addon/modifiers/in-viewport.js
+++ addon/modifiers/in-viewport.js
@@ -4,92 +4,184 @@ import { inject as service } from '@ember/service';
 import { DEBUG } from '@glimmer/env';
 import Modifier from 'ember-modifier';
 import deepEqual from 'fast-deep-equal';
+import { registerDestructor } from '@ember/destroyable';
+import { macroCondition, dependencySatisfies } from '@embroider/macros';
 
 const WATCHED_ELEMENTS = DEBUG ? new WeakSet() : undefined;
 
-export default class InViewportModifier extends Modifier {
-  @service inViewport;
+let modifier;
 
-  name = 'in-viewport';
+if (macroCondition(dependencySatisfies('ember-modifier', '>=3.2.0 || 4.x'))) {
+  modifier = class InViewportModifier extends Modifier {
+    @service inViewport;
 
-  lastOptions;
+    name = 'in-viewport';
 
-  get options() {
-    // eslint-disable-next-line no-unused-vars
-    const { onEnter, onExit, ...options } = this.args.named;
-    return options;
-  }
+    lastOptions;
+    element = null;
 
-  get hasStaleOptions() {
-    return !deepEqual(this.options, this.lastOptions);
-  }
+    modify(element, positional, named) {
+      this.element = element;
+      this.positional = positional;
+      this.named = named;
+      this.validateArguments();
 
-  validateArguments() {
-    assert(
-      `'{{in-viewport}}' does not accept positional parameters. Specify listeners via 'onEnter' / 'onExit'.`,
-      this.args.positional.length === 0
-    );
-    assert(
-      `'{{in-viewport}}' either expects 'onEnter', 'onExit' or both to be present.`,
-      typeof this.args.named.onEnter === 'function' ||
-        typeof this.args.named.onExit === 'function'
-    );
-  }
-
-  @action
-  onEnter(...args) {
-    if (this.args.named.onEnter) {
-      this.args.named.onEnter.call(null, this.element, ...args);
+      if (!this.didSetup) {
+        this.setupWatcher(element);
+        registerDestructor(() => this.destroyWatcher(element));
+      } else if (this.hasStaleOptions) {
+        this.destroyWatcher(element);
+        this.setupWatcher(element);
+      }
     }
 
-    if (!this.options.viewportSpy) {
+    get options() {
+      // eslint-disable-next-line no-unused-vars
+      const { onEnter, onExit, ...options } = this.named;
+      return options;
+    }
+
+    get hasStaleOptions() {
+      return !deepEqual(this.options, this.lastOptions);
+    }
+
+    validateArguments() {
+      assert(
+        `'{{in-viewport}}' does not accept positional parameters. Specify listeners via 'onEnter' / 'onExit'.`,
+        this.positional.length === 0
+      );
+      assert(
+        `'{{in-viewport}}' either expects 'onEnter', 'onExit' or both to be present.`,
+        typeof this.named.onEnter === 'function' ||
+          typeof this.named.onExit === 'function'
+      );
+    }
+
+    @action
+    onEnter(...args) {
+      if (this.named.onEnter) {
+        this.named.onEnter.call(null, this.element, ...args);
+      }
+
+      if (!this.options.viewportSpy) {
+        this.inViewport.stopWatching(this.element);
+      }
+    }
+
+    @action
+    onExit(...args) {
+      if (this.named.onExit) {
+        this.named.onExit.call(null, this.element, ...args);
+      }
+    }
+
+    setupWatcher(element) {
+      assert(
+        `'${element}' is already being watched. Make sure that '{{in-viewport}}' is only used once on this element and that you are not calling 'inViewport.watchElement(element)' in other places.`,
+        !WATCHED_ELEMENTS.has(element)
+      );
+      if (DEBUG) WATCHED_ELEMENTS.add(element);
+      this.inViewport.watchElement(
+        element,
+        this.options,
+        this.onEnter,
+        this.onExit
+      );
+      this.lastOptions = this.options;
+    }
+
+    destroyWatcher(element) {
+      if (DEBUG) WATCHED_ELEMENTS.delete(element);
+      this.inViewport.stopWatching(element);
+    }
+  };
+} else {
+  modifier = class InViewportModifier extends Modifier {
+    @service inViewport;
+
+    name = 'in-viewport';
+
+    lastOptions;
+
+    get options() {
+      // eslint-disable-next-line no-unused-vars
+      const { onEnter, onExit, ...options } = this.args.named;
+      return options;
+    }
+
+    get hasStaleOptions() {
+      return !deepEqual(this.options, this.lastOptions);
+    }
+
+    validateArguments() {
+      assert(
+        `'{{in-viewport}}' does not accept positional parameters. Specify listeners via 'onEnter' / 'onExit'.`,
+        this.args.positional.length === 0
+      );
+      assert(
+        `'{{in-viewport}}' either expects 'onEnter', 'onExit' or both to be present.`,
+        typeof this.args.named.onEnter === 'function' ||
+          typeof this.args.named.onExit === 'function'
+      );
+    }
+
+    @action
+    onEnter(...args) {
+      if (this.args.named.onEnter) {
+        this.args.named.onEnter.call(null, this.element, ...args);
+      }
+
+      if (!this.options.viewportSpy) {
+        this.inViewport.stopWatching(this.element);
+      }
+    }
+
+    @action
+    onExit(...args) {
+      if (this.args.named.onExit) {
+        this.args.named.onExit.call(null, this.element, ...args);
+      }
+    }
+
+    setupWatcher() {
+      assert(
+        `'${this.element}' is already being watched. Make sure that '{{in-viewport}}' is only used once on this element and that you are not calling 'inViewport.watchElement(element)' in other places.`,
+        !WATCHED_ELEMENTS.has(this.element)
+      );
+      if (DEBUG) WATCHED_ELEMENTS.add(this.element);
+      this.inViewport.watchElement(
+        this.element,
+        this.options,
+        this.onEnter,
+        this.onExit
+      );
+      this.lastOptions = this.options;
+    }
+
+    destroyWatcher() {
+      if (DEBUG) WATCHED_ELEMENTS.delete(this.element);
       this.inViewport.stopWatching(this.element);
     }
-  }
 
-  @action
-  onExit(...args) {
-    if (this.args.named.onExit) {
-      this.args.named.onExit.call(null, this.element, ...args);
-    }
-  }
-
-  setupWatcher() {
-    assert(
-      `'${this.element}' is already being watched. Make sure that '{{in-viewport}}' is only used once on this element and that you are not calling 'inViewport.watchElement(element)' in other places.`,
-      !WATCHED_ELEMENTS.has(this.element)
-    );
-    if (DEBUG) WATCHED_ELEMENTS.add(this.element);
-    this.inViewport.watchElement(
-      this.element,
-      this.options,
-      this.onEnter,
-      this.onExit
-    );
-    this.lastOptions = this.options;
-  }
-
-  destroyWatcher() {
-    if (DEBUG) WATCHED_ELEMENTS.delete(this.element);
-    this.inViewport.stopWatching(this.element);
-  }
-
-  didInstall() {
-    this.setupWatcher();
-  }
-
-  didUpdateArguments() {
-    if (this.hasStaleOptions) {
-      this.destroyWatcher();
+    didInstall() {
       this.setupWatcher();
     }
-  }
 
-  didReceiveArguments() {
-    this.validateArguments();
-  }
+    didUpdateArguments() {
+      if (this.hasStaleOptions) {
+        this.destroyWatcher();
+        this.setupWatcher();
+      }
+    }
 
-  willRemove() {
-    this.destroyWatcher();
-  }
+    didReceiveArguments() {
+      this.validateArguments();
+    }
+
+    willRemove() {
+      this.destroyWatcher();
+    }
+  };
 }
+
+export default modifier;
